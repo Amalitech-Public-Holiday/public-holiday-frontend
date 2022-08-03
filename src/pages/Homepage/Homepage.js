c9bef8ce2512addefc25440e6c77640afb669fd1
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-03 21:00:22 +0000

    Add sigup and login page buttons

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -1,10 +1,17 @@
 import "./Homepage.scss";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
     return (
       <div className="Homepage">
-          <h1>Welcome to Public Holidays</h1>
-          <p>Discover holidays in countries with a month and/or day in a year.</p>
+        <h1>Welcome to React Public Holidays</h1>
+        <p>Discover holidays in countries with a month and/or day in a year.</p>
+        <div className="buttons">
+          <div className="row">
+            <Link to="#" className="button blue column">Signup</Link>
+            <Link to="#" className="button column">Login</Link>
+          </div>
+        </div>
       </div>
     );
 }
