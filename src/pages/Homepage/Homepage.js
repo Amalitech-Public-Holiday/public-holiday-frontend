c32aef60aaf21b16384be9f9b86d1f2534c2a1a7
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-03 17:09:59 +0000

    Add app subtitle and center

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -4,6 +4,7 @@ const Homepage = () => {
     return (
       <div className="Homepage">
           <h1>Welcome to Public Holidays</h1>
+          <p>Discover holidays in countries with a month and/or day in a year.</p>
       </div>
     );
 }
