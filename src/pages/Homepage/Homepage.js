168bbc550b2dac678938b2596ee7188ce5faa1c6
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-03 16:49:58 +0000

    Create Homepage component and add style

---
+++ src/pages/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import "./Homepage.scss";
+
+const Homepage = () => {
+    return (
+      <div className="Homepage">
+          <h1>Welcome to Public Holidays</h1>
+      </div>
+    );
+}
+
+export default Homepage;
