c88602147fd76bb296006c2bec800c4438c95456
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-04 07:08:05 +0000

    Add signup page route

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import "normalize-css";
 import "./App.scss";
-import Homepage from "./pages/Homepage/Homepage";
 import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage/Homepage";
+import Signup from "./pages/Signup/Signup";
 
 function App() {
   return (
     <div className='App'>
       <Routes>
-        <Route path="/" element={<Homepage/>}/>
+        <Route exact path="/" element={<Homepage/>}/>
+        <Route path="/signup" element={<Signup/>}/>
       </Routes>
     </div>
   );
