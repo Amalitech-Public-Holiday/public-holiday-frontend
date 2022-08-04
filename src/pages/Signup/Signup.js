343e18702392c7eb832bc49c13bc0459bda46b92
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-04 07:20:27 +0000

    Create Signup component and import sass styles

---
+++ src/pages/Signup/Signup.js
@@ -0,0 +1,11 @@
+import "./Signup.scss";
+
+const Signup = () => {
+    return (
+        <div className="Signup">
+            <h2>Signup Page</h2>
+        </div>
+    );
+}
+
+export default Signup;
