591e6fd733479c688dcb7dfb125b0d13b1447746
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-04 11:10:55 +0000

    Add NavBar with Login page link

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,8 +1,13 @@
 import "./Signup.scss";
+import NavBar from "../../components/NavBar/NavBar";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
     return (
         <div className="Signup">
+            <NavBar>
+                <Link to="/login">Login</Link>
+            </NavBar>
             <h2>Signup Page</h2>
         </div>
     );
