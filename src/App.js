b8c42984ea4f269decb43906a87963b80cbffa95
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-04 07:30:49 +0000

    Add Login page route link to login button

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import Signup from "./pages/Signup/Signup";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Routes>
-        <Route exact path="/" element={<Homepage/>}/>
-        <Route path="/signup" element={<Signup/>}/>
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
