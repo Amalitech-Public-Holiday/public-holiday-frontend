3afaff5801215515d641aa24cb9543724eba2622
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-05 20:06:22 +0000

    Add SignupForm component

--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,6 +1,7 @@
 import "./Signup.scss";
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
+import SignupForm from "../../components/SignupForm/SignupForm";
 import { Link } from "react-router-dom";
 
 const Signup = () => {
@@ -10,6 +11,7 @@ const Signup = () => {
                 <Link to="/login">Login</Link>
             </NavBar>
             <h2>Create an account</h2>
+            <SignupForm/>
             <Footer/>
         </div>
     );
