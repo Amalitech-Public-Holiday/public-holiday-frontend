55fbf74ca504f46b988e122e26efd8e4d3c6a1e2
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-05 20:18:06 +0000

    Add inputs and labels for fullname, email and passwords details

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -1,8 +1,23 @@
 const SignupForm = () => {
-    return(
+    return (
+      <div>
         <div>
-
+          <label htmlFor="fullname">Full name:</label>
+          <input type="text" name="fullname" />
         </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="password1">Password:</label>
+          <input type="password" name="password1" />
+        </div>
+        <div>
+          <label htmlFor="password2">Retype password:</label>
+          <input type="password" name="password2" />
+        </div>
+      </div>
     );
 }
 
