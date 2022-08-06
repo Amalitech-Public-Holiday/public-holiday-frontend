2d6598b7a2b507d9069cd5dbfd6d32690ac8bea0
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-06 14:11:55 +0000

    Change Homepage link to Home

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,16 +1,15 @@
 import "./NavBar.scss";
-
 import { NavLink } from "react-router-dom";
 
 const NavBar = (props) => {
-    return(
-        <div className="NavBar">
-            <nav>
-                <NavLink to="/">Homepage</NavLink>
-                {props.children}
-            </nav>
-        </div>
-    );
-}
+  return (
+    <div className="NavBar">
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        {props.children}
+      </nav>
+    </div>
+  );
+};
 
 export default NavBar;
