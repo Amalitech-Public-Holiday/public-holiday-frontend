fcf05bd25cad964638d5488c15512343d6fc27eb
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-06 19:23:58 +0000

    Import and use LoginForm component

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,6 +1,7 @@
 import "./Login.scss";
 import NavBar from "../../components/NavBar/NavBar";
 import Footer from "../../components/Footer/Footer";
+import LoginForm from "../../components/LoginForm/LoginForm";
 import { Link } from "react-router-dom";
 
 const Login = () => {
@@ -10,6 +11,7 @@ const Login = () => {
                 <Link to="/signup">Signup</Link>
             </NavBar>
             <h2>Login to access</h2>
+            <LoginForm/>
             <Footer/>
         </div>
     );
