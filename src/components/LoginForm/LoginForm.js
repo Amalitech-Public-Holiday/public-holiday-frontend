3b43d6c01964d6d58ebad919da3c82dc5ce5b490
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-06 19:29:46 +0000

    Add form with inputs for email and password and submit button

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,7 +1,19 @@
 const LoginForm = () => {
   return (
-    <div>
-      
+    <div className="LoginForm">
+      <form>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="fullname" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password" />
+        </div>
+        <div>
+          <input type="submit" className="button green" />
+        </div>
+      </form>
     </div>
   );
 };
