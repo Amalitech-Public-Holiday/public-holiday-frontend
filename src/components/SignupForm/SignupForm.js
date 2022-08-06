18c3d16465245d3347c5781295a2c460471f360e
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-06 20:11:02 +0000

    Add autocomplete and autofocus to form and inputs elements

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -3,10 +3,10 @@ import "./SignupForm.scss";
 const SignupForm = () => {
   return (
     <div className="SignupForm">
-      <form>
+      <form autoComplete="true">
         <div>
           <label htmlFor="fullname">Full name:</label>
-          <input type="text" name="fullname" />
+          <input type="text" name="fullname" autoFocus={true}/>
         </div>
         <div>
           <label htmlFor="email">Email:</label>
@@ -14,11 +14,11 @@ const SignupForm = () => {
         </div>
         <div>
           <label htmlFor="password1">Password:</label>
-          <input type="password" name="password1" />
+          <input type="password" name="password1" autoComplete="true"/>
         </div>
         <div>
           <label htmlFor="password2">Retype password:</label>
-          <input type="password" name="password2" />
+          <input type="password" name="password2" autoComplete="true"/>
         </div>
         <div>
           <input type="submit" className="button green" />
