decb78e099a897bdb5422bbd718c8c5f164fef9c
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-07 20:00:47 +0000

    Set password input minimum length constraint

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -7,7 +7,7 @@ const SignupForm = () => {
   const [email, setEmail] = useState("");
   const [password1, setPassword1] = useState("");
   const [password2, setPassword2] = useState("");
-
+  
   const handleFullname = ({target}) => {
     setFullname(target.value);
   }
@@ -23,24 +23,55 @@ const SignupForm = () => {
   const handlePassword2 = ({target}) => {
     setPassword2(target.value);
   }
+
   return (
     <div className="SignupForm">
       <form autoComplete="true">
         <div>
           <label htmlFor="fullname">Full name:</label>
-          <input type="text" name="fullname" autoFocus={true} value={fullname} onChange={handleFullname} required/>
+          <input
+            type="text"
+            name="fullname"
+            value={fullname}
+            autoFocus={true}
+            onChange={handleFullname}
+            required
+          />
+          <span>{}</span>
         </div>
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="email" name="email" value={email} onChange={handleEmail} required/>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleEmail}
+            required
+          />
         </div>
         <div>
           <label htmlFor="password1">Password:</label>
-          <input type="password" name="password1" autoComplete="true" value={password1} onChange={handlePassword1} required/>
+          <input
+            type="password"
+            name="password1"
+            value={password1}
+            autoComplete="true"
+            onChange={handlePassword1}
+            minLength="12"
+            required
+          />
         </div>
         <div>
           <label htmlFor="password2">Retype password:</label>
-          <input type="password" name="password2" autoComplete="true" value={password2} onChange={handlePassword2} required/>
+          <input
+            type="password"
+            name="password2"
+            value={password2}
+            autoComplete="true"
+            onChange={handlePassword2}
+            minLength="12"
+            required
+          />
         </div>
         <div>
           <input type="submit" className="button green" />
