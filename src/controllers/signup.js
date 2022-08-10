25bbb5412a2a1e9bc658747e4b200452b6ceda0e
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-10 21:22:29 +0000

    Create async function to find email in database

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,1 +1,7 @@
 const BASE_URL = "https://react-public-holidays-backend.herokuapp.com";
+
+export const getUserByEmail = async (email) => {
+  const response = await fetch(`${BASE_URL}/users?email=${email}`);
+  const jsonResponse = await response.json();
+  return jsonResponse;
+};
