026b34a70a5571152b632540499f386c38c16560
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-11 08:08:14 +0000

    Add onChange handler to email input to set email state

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -4,13 +4,17 @@ import { useState } from "react";
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  
+
+  const handleEmail = ({target}) => {
+    setEmail(target.value);  
+  }
+
   return (
     <div className="LoginForm">
       <form>
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="email" name="fullname" />
+          <input type="email" name="email" value={email} onChange={handleEmail} autoFocus/>
         </div>
         <div>
           <label htmlFor="password">Password:</label>
