a948613389d42bea63100c6231a0d5cf97d80de4
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-11 12:59:12 +0000

    Use isDisabled state variable to toggle input disabled attributes

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -5,6 +5,7 @@ import LoginUser from "../../controllers/login";
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [isDisabled, setIsDisabled] = useState(false);
 
   const handleEmail = ({target}) => {
     setEmail(target.value);  
@@ -18,17 +19,21 @@ const LoginForm = () => {
     e.preventDefault();
 
     if (email && password !== '') {
+      setIsDisabled(true);
       LoginUser(email, password).then(result => {
         if (result.status) {
           alert(`User ${result.username} has logged in successfully!`);
+          setIsDisabled(false);
           setEmail('');
           setPassword('');
         } else if (result.error404) {
           alert(result.error404);
+          setIsDisabled(false);
         } else {
           alert(result.error500);
+          setIsDisabled(false);
         }
-      })
+      }).catch(error => console.log(error));
     }
   }
   return (
@@ -36,14 +41,29 @@ const LoginForm = () => {
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="email">Email:</label>
-          <input type="email" name="email" value={email} onChange={handleEmail} autoFocus required/>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={handleEmail}
+            autoFocus
+            required
+            disabled={isDisabled}
+          />
         </div>
         <div>
           <label htmlFor="password">Password:</label>
-          <input type="password" name="password" value={password} onChange={handlePassword} required/>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handlePassword}
+            required
+            disabled={isDisabled}
+          />
         </div>
         <div>
-          <button type="submit" className="button green">
+          <button type="submit" className="button green" disabled={isDisabled}>
             Submit
           </button>
         </div>
