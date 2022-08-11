390d064e651c370127d1a1759b6daaab67c833e5
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-11 22:41:33 +0000

    Implement a logout logic

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,17 @@
-import { useSelector } from "react-redux/";
+import { useSelector, useDispatch } from "react-redux/";
+import { logout } from "../../features/AuthSlice";
 
 const Dashboard = () => {
-    const auth = useSelector((state) => state.auth);
+const auth = useSelector((state) => state.auth);
+const dispatch = useDispatch();
 
+const handleLogout = () => {
+    dispatch(logout());
+}
   return (
     <div>
       <h1>Welcome, {auth.authStatus && auth.username}</h1>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
