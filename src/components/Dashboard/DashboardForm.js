4123eea9394cdc977fd9b80d4dd14ebbfc997feb
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-12 22:21:56 +0000

    Move generateDays functions to util folder in util file and import

--- src/components/Dashboard/DashboardForm.js
+++ src/components/Dashboard/DashboardForm.js
@@ -1,23 +1,12 @@
 import { useState } from "react";
+import { generateDays } from "../../util/util";
 
 const DashboardForm = () => {
   const [country, setCountry] = useState('');
   const [year, setYear] = useState('');
   const [month, setMonth] = useState('');
   const [day, setDay] = useState('');
-  
-  const generateDays = () => {
-      const days = [];
-      for (let i = 0; i < 32; i++) {
-          if (i === 0) {
-            days.push(<option value='' key={0}>Any day</option>);
-            continue;
-          }
-          const number = i <= 9 ? `0${i}` : i;
-          days.push(<option value={i} key={i}>{number}</option>);
-      }
-      return days;
-  }
+
   return (
     <div>
       <select name="country">
