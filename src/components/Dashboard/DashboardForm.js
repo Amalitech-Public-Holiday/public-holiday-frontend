b9c3b86c06bdf5088bffda03a6e611d29e818ffd
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-12 20:25:40 +0000

    Add dropdwon input with default empty and all months options

--- src/components/Dashboard/DashboardForm.js
+++ src/components/Dashboard/DashboardForm.js
@@ -2,16 +2,31 @@ const DashboardForm = () => {
     return (
       <div>
         <select name="country">
-          <option selected>All countries</option>
+          <option value="" selected>All countries</option>
           <option value="GH">Ghana</option>
           <option value="DE">Germany</option>
           <option value="RW">Rwanda</option>
         </select>
         <select name="year">
           <option value="2022" selected>2022</option>
-          <option value="2023">2023</option>s
+          <option value="2023">2023</option>
           <option value="2024">2024</option>
           <option value="2025">2025</option>
+        </select>
+        <select name="month">
+          <option value="" selected>Any month</option>
+          <option value="01">January</option>
+          <option value="02">February</option>
+          <option value="03">March</option>
+          <option value="04">April</option>
+          <option value="05">May</option>
+          <option value="06">June</option>
+          <option value="07">July</option>
+          <option value="08">August</option>
+          <option value="09">September</option>
+          <option value="10">October</option>
+          <option value="11">November</option>
+          <option value="12">December</option>
         </select>
       </div>
     );
