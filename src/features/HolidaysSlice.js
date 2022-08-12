30ef43f754746feb9ed83bfeb313ffda4114fbf3
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-12 22:02:06 +0000

    Create and export an async fetch holidays thunk

--- src/features/HolidaysSlice.js
+++ src/features/HolidaysSlice.js
@@ -1,4 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { getHolidays } from "../controllers/holidaysAPI";
+
+export const asyncFetchHolidays = createAsyncThunk(
+    'holidays/getHolidays',
+    async (queryString, thunkAPI) => {
+        const data = await getHolidays(queryString);
+        return data;
+    }
+);
 
 export const holidays = createSlice({
     name: 'holidays',
