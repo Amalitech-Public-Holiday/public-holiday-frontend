550f8f82ff4865521d28307d693c414955268b83
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-12 22:41:39 +0000

    Update generateDays to set default value for no selection

--- src/util/util.js
+++ src/util/util.js
@@ -1,12 +1,12 @@
-export const generateDays = () => {
+export const generateDays = (defaultValue) => {
       const days = [];
       for (let i = 0; i < 32; i++) {
           if (i === 0) {
-            days.push(<option value='' key={0}>Any day</option>);
+            days.push(<option value={defaultValue} key={0}>Any day</option>);
             continue;
           }
           const number = i <= 9 ? `0${i}` : i;
-          days.push(<option value={i} key={i}>{number}</option>);
+          days.push(<option value={number} key={i}>{number}</option>);
       }
       return days;
   }
