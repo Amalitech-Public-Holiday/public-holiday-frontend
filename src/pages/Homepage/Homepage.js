187332ce0a4da83b3323d28dffadf5c889824877
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-12 09:26:47 +0000

    Update subheading and Signup button class to green

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -6,10 +6,10 @@ const Homepage = () => {
     return (
       <div className="Homepage">
         <h1>Welcome to React Public Holidays</h1>
-        <p>Discover holidays in countries with a month and/or day in a year.</p>
+        <p>Discover holidays in a month, day or year in Ghana, Germany and Rwanda.</p>
         <div className="buttons">
           <div className="row">
-            <Link to="/signup" className="button blue column">Signup</Link>
+            <Link to="/signup" className="button green column">Signup</Link>
             <Link to="/login" className="button column">Login</Link>
           </div>
         </div>
