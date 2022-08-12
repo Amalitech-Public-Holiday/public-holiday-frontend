493fc7fde549ff7bd9386bc9dcf17dba2c37a448
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-12 20:06:34 +0000

    Add country dropdown with default and all countries options

--- src/components/Dashboard/DashboardForm.js
+++ src/components/Dashboard/DashboardForm.js
@@ -1,9 +1,14 @@
 const DashboardForm = () => {
-    return(
-        <div>
-            <h1>Welcome to Dashboard</h1>
-        </div>
-    )
+    return (
+      <div>
+        <select name="country">
+          <option selected>All countries</option>
+          <option value="GH">Ghana</option>
+          <option value="DE">Germany</option>
+          <option value="RW">Rwanda</option>
+        </select>
+      </div>
+    );
 }
 
 export default DashboardForm;
