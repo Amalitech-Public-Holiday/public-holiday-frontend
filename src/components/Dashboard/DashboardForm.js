89ea64b0e08a2d703561c29d39b83117296f78aa
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-12 20:13:32 +0000

    Add dropdown for year with default value 2022

--- src/components/Dashboard/DashboardForm.js
+++ src/components/Dashboard/DashboardForm.js
@@ -7,6 +7,12 @@ const DashboardForm = () => {
           <option value="DE">Germany</option>
           <option value="RW">Rwanda</option>
         </select>
+        <select name="year">
+          <option value="2022" selected>2022</option>
+          <option value="2023">2023</option>s
+          <option value="2024">2024</option>
+          <option value="2025">2025</option>
+        </select>
       </div>
     );
 }
