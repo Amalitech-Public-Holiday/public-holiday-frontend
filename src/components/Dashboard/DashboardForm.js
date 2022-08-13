fe410edec3e64a7903c6cbb2902d0d8f625e33fd
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-13 11:25:04 +0000

    Add a div wrapper around inputs

--- src/components/Dashboard/DashboardForm.js
+++ src/components/Dashboard/DashboardForm.js
@@ -42,37 +42,47 @@ const DashboardForm = () => {
 
   return (
     <div className="DashboardForm">
-      <select name="country" onChange={handleCountry}>
-        <option value=''>Any country</option>
-        <option value="GH">Ghana</option>
-        <option value="DE">Germany</option>
-        <option value="RW">Rwanda</option>
-      </select>
-      <select name="year" onChange={handleYear}>
-        <option value='2022'>2022</option>
-        <option value="2023">2023</option>
-        <option value="2024">2024</option>
-        <option value="2025">2025</option>
-      </select>
-      <select name="month" onChange={handleMonth}>
-        <option value=''>Any month</option>
-        <option value="01">January</option>
-        <option value="02">February</option>
-        <option value="03">March</option>
-        <option value="04">April</option>
-        <option value="05">May</option>
-        <option value="06">June</option>
-        <option value="07">July</option>
-        <option value="08">August</option>
-        <option value="09">September</option>
-        <option value="10">October</option>
-        <option value="11">November</option>
-        <option value="12">December</option>
-      </select>
-      <select name="day" onChange={handleDay}>
+      <div>
+        <select name="country" onChange={handleCountry}>
+          <option value=''>Any country</option>
+          <option value="GH">Ghana</option>
+          <option value="DE">Germany</option>
+          <option value="RW">Rwanda</option>
+        </select>
+      </div>
+      <div>
+        <select name="year" onChange={handleYear}>
+          <option value='2022'>2022</option>
+          <option value="2023">2023</option>
+          <option value="2024">2024</option>
+          <option value="2025">2025</option>
+        </select>
+      </div>
+      <div>
+        <select name="month" onChange={handleMonth}>
+          <option value=''>Any month</option>
+          <option value="01">January</option>
+          <option value="02">February</option>
+          <option value="03">March</option>
+          <option value="04">April</option>
+          <option value="05">May</option>
+          <option value="06">June</option>
+          <option value="07">July</option>
+          <option value="08">August</option>
+          <option value="09">September</option>
+          <option value="10">October</option>
+          <option value="11">November</option>
+          <option value="12">December</option>
+        </select>
+      </div>
+      <div>
+        <select name="day" onChange={handleDay}>
           {generateDays()}
-      </select>
-      <button type="submit" onClick={handleSubmit}>Submit</button>
+        </select>
+      </div>
+      <div>
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+      </div>
     </div>
   );
 }
