f56725d21c28b98a39ca1d4c7ce66a513fc02761
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-13 10:29:48 +0000

    Remove generateDays parameter

--- src/util/util.js
+++ src/util/util.js
@@ -1,8 +1,8 @@
-export const generateDays = (defaultValue) => {
+export const generateDays = () => {
       const days = [];
       for (let i = 0; i < 32; i++) {
           if (i === 0) {
-            days.push(<option value={defaultValue} key={0}>Any day</option>);
+            days.push(<option value='' key={0}>Any day</option>);
             continue;
           }
           const number = i <= 9 ? `0${i}` : i;
