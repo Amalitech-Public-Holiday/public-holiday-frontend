19ee79ea16f57b75a9329a9c4d172632d7fd3911
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-13 10:34:19 +0000

    Implement handleSumbit to generate query string

--- src/components/Dashboard/DashboardForm.js
+++ src/components/Dashboard/DashboardForm.js
@@ -3,7 +3,7 @@ import { generateDays } from "../../util/util";
 
 const DashboardForm = () => {
   const [country, setCountry] = useState('');
-  const [year, setYear] = useState('');
+  const [year, setYear] = useState('2022');
   const [month, setMonth] = useState('');
   const [day, setDay] = useState('');
 
@@ -23,22 +23,38 @@ const DashboardForm = () => {
     setDay(target.value);
   };
 
+  const handleSubmit = () => {
+    let queries = '';
+    if (country !== "") {
+      queries += `&country=${country}`;
+    }
+    if (year !== "") {
+      queries += `&year=${year}`;
+    }
+    if (month !== "") {
+      queries += `&month=${month}`;
+    }
+    if (day !== "") {
+      queries += `&day=${day}`;
+    }
+  }
+
   return (
-    <div>
+    <div className="DashboardForm">
       <select name="country" onChange={handleCountry}>
-        <option value="">All Countries</option>
+        <option value=''>Any country</option>
         <option value="GH">Ghana</option>
         <option value="DE">Germany</option>
         <option value="RW">Rwanda</option>
       </select>
       <select name="year" onChange={handleYear}>
-        <option value="2022">2022</option>
+        <option value='2022'>2022</option>
         <option value="2023">2023</option>
         <option value="2024">2024</option>
         <option value="2025">2025</option>
       </select>
       <select name="month" onChange={handleMonth}>
-        <option value="">Any month</option>
+        <option value=''>Any month</option>
         <option value="01">January</option>
         <option value="02">February</option>
         <option value="03">March</option>
@@ -55,7 +71,7 @@ const DashboardForm = () => {
       <select name="day" onChange={handleDay}>
           {generateDays()}
       </select>
-      <button>Submit</button>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
