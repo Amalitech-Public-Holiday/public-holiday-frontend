f730782c8f61e641090e7f00f4c25eda4a6ccc0e
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-13 13:13:33 +0000

    Import DashboardResults scss

--- src/components/Dashboard/DashboardResults/DashboardResults.js
+++ src/components/Dashboard/DashboardResults/DashboardResults.js
@@ -1,9 +1,11 @@
+import "./DashboardResults.scss";
+
 const DashboardResults = () => {
-    return(
-        <div>
-            Dashboard Search Results
-        </div>
-    );
+  return(
+    <div>
+        Dashboard Search Results
+    </div>
+  );
 }
 
 export default DashboardResults;
