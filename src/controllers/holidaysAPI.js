c2f069d2b0ca5f3c3fc628ebd77c60a08fd24704
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-13 12:44:23 +0000

    Update API url

--- src/controllers/holidaysAPI.js
+++ src/controllers/holidaysAPI.js
@@ -1,6 +1,6 @@
 export const getHolidays = (queryString) => {
     try {
-        const response = fetch(`https://holidays.abstractapi.com/v1/?${process.env.REACT_APP_API_KEY}&${queryString}`);
+        const response = fetch(`https://holidays.abstractapi.com/v1/?api_key${process.env.REACT_APP_API_KEY}${queryString}`);
         return response.json();
     } catch(error) {
         console.log(error);
