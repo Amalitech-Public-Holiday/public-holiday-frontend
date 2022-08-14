f1da98b05914d4ddbb4fa8832481d0f6508651ed
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-14 16:05:30 +0000

    Reduce tab spacing

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -5,17 +5,17 @@ import LoginForm from "../../components/LoginForm/LoginForm";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-      <div className="Login">
-        <NavBar>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/signup">Signup</Link></li>
-        </NavBar>
-        <h2 className="text-xlarge">Login to access</h2>
-        <LoginForm />
-        <Footer />
-      </div>
-    );
+  return (
+    <div className="Login">
+      <NavBar>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/signup">Signup</Link></li>
+      </NavBar>
+      <h2 className="text-xlarge">Login to access</h2>
+      <LoginForm />
+      <Footer />
+    </div>
+  );
 }
 
 export default Login;
