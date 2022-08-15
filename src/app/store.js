2205361ac6abc53e48de5fed82122c8914b11622
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-15 21:12:18 +0000

    Import and use holiday reducer

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import AuthSlice from '../features/AuthSlice';
+import HolidaysSlice from '../features/HolidaysSlice';
+
 export const store = configureStore({
   reducer: {
-    auth: AuthSlice
+    auth: AuthSlice,
+    holiday: HolidaysSlice
   },
 });
