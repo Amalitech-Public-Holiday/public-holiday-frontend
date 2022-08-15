c3597c2f583da0758f50742ecd41900a05ec07e6
prince-t-asamoah <93658262+prince-asamoah-amalitech@users.noreply.github.com>
2022-08-15 04:09:11 +0000

    Hard code api key

--- src/controllers/holidaysAPI.js
+++ src/controllers/holidaysAPI.js
@@ -1,6 +1,8 @@
 export const getHolidays = (queryString) => {
     try {
-        const response = fetch(`https://holidays.abstractapi.com/v1/?api_key${process.env.REACT_APP_API_KEY}${queryString}`);
+        const response = fetch(
+          `https://holidays.abstractapi.com/v1/?api_key=8b5a95ad022b47939ec50054a90d651c${queryString}`
+        );
         return response.json();
     } catch(error) {
         console.log(error);
